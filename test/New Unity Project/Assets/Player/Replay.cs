80a64583c8d64338744dd4983bb043a3b9ae53a3
Popfer <32405025+Popfer@users.noreply.github.com>
2018-04-05 17:01:53 -1000

    Continue

---
+++ test/New Unity Project/Assets/Player/Replay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Replay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MyKeyFrame testKey = new MyKeyFrame(1.0f, Vector3.up, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public struct MyKeyFrame {
+    public float frameTime;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame (float aTime, Vector3 aPosition, Quaternion aRotation) {
+        frameTime = aTime;
+        position = aPosition;
+        rotation = aRotation;
+    }
+}
